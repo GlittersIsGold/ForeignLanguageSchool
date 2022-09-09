e16cd1b6b6f79018db3979a26e2e34b60aa648a0
Kirill <cyberk1ra@yandex.ru>
2022-09-09 19:45:20 +0300

    session 1 2/3 done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ForeignLanguagesSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShowTwoHundred_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShowFifthy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShowTen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFilterAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFilterByFamily_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFilterByGender_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFilterByLastVisit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
